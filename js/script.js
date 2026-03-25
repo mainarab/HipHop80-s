8c2793a86397b6a0e724661a505799bec5be3e62
Mainara Barbosa <206761415+mainarab@users.noreply.github.com>
2026-03-25 16:56:56 -0300

    Projeto

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+    const destino = this.getAttribute('href');
+        if (destino && destino.startsWith('#')) {
+            e.preventDefault();
+    const elemento = document.querySelector(destino);
+            if (elemento) {
+                elemento.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }   
+        }
+    });
+});
+
+
+const header = document.querySelector(".header");
+
+function mudarHeader(){
+    header.classList.toggle('ativo', window.scrollY > 40);
+}
+
+window.addEventListener("scroll", mudarHeader);
+
+
+
+
+
+
